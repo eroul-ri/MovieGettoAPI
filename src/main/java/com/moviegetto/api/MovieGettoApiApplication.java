419f87976bd63fbe3d7c11cbca449a8ff5f8caa2
eroul-ri <bhr139313@gmail.com>
2023-08-23 11:21:43 +0900

    inital

---
+++ src/main/java/com/moviegetto/api/MovieGettoApiApplication.java
@@ -0,0 +1,13 @@
+package com.moviegetto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGettoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieGettoApiApplication.class, args);
+    }
+
+}
