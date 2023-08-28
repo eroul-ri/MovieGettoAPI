a37a7b29b663278f6c87b850b38ae46e60888df7
eroul-ri <bhr139313@gmail.com>
2023-08-28 16:18:37 +0900

    [hr] APIResponse, ErrorResponse

---
+++ src/main/java/com/moviegetto/api/common/dto/ErrorField.java
@@ -0,0 +1,11 @@
+package com.moviegetto.api.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErrorField {
+	private String name;
+	private String reason;
+}
